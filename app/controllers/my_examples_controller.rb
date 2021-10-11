6ec048371ae9ee273fdbb942548d874f53e78afb
Ian S <iansilber32@gmail.com>
2021-10-11 14:50:42 -0400

    creates fortune/lotto/bottles of beer web app

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,36 @@
 class MyExamplesController < ApplicationController
+  def fortunes
+    array = [
+      "you will soon be met with great success.",
+      "friends are worth more than money!",
+      "today is a good day to spend with family",
+    ]
+    render html: array.sample
+  end
+
+  def lotto_nums
+    array = (1..60).to_a
+    lotto_nums = []
+    6.times do
+      lotto_nums << array.sample
+    end
+    render html: "your winning lottery numbers are: #{lotto_nums}"
+  end
+
+  def b_o_b
+    i = 100
+    array = []
+    while i >= 0
+      if i > 0
+        array << "#{i} bottles of beer on the wall, #{i} bottles of beer"
+        array << "take one down pass it around, #{(i - 1)} bottles of beer on the wall"
+      elsif i == 0
+        array << "0 bottles of beer on the wall, 0 bottles of beer"
+        array << "go to the store and get some more, 99 bottles of beer on the wall"
+      end
+      i -= 1
+    end
+    song = array.join(" ")
+    render html: song
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/fortunes", controller: "my_examples", action: "fortunes"
+  get "/lotto_nums", controller: "my_examples", action: "lotto_nums"
+  get "/b_o_b", controller: "my_examples", action: "b_o_b"
 end
